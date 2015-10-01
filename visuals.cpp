a8d98c3bcc55e8f6c9c75a27fc05a10ed686166d
BourgondAries <macocio@gmail.com>
2015-10-01 16:13:38 +0200

    Add color buffer and finish task 3

--- visuals.cpp
+++ visuals.cpp
@@ -26,7 +26,7 @@ namespace ttl {
 
 // This will be used with shader
 //GLuint VertexArrayID;
-GLuint vertexbuffer, colorbuffer, legbuffer, legcolorbuffer;
+GLuint vertexbuffer, colorbuffer, legbuffer, legcolorbuffer, hipcolorbuffer;
 GLuint objvertexbuffer; // for obj
 GLuint programID_1, programID_2;
 
@@ -545,6 +545,8 @@ void RenderScene5()
 	// ========== HIP JOINT 1
 	// =================================================================================
 	//transform the cube
+	glBindBuffer(GL_ARRAY_BUFFER, hipcolorbuffer);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
 	Model      = glm::translate(glm::mat4(1.0f),glm::vec3(1,0,0));
 	Model      = glm::scale(Model,glm::vec3(0.2,0.2,0.2));
 	// MVP
@@ -696,13 +698,12 @@ void SetupGL() //
 		0.f, -1.f, 0.f,
 	};
 	static const GLfloat g_vertex_buffer_data_leg_color[] = {
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-
-		0.327f,  0.483f,  0.844f,
-		0.609f,  0.115f,  0.436f,
-		0.583f,  0.771f,  0.014f,
+		1.f, 1.f, 0.f,
+		1.f, 1.f, 0.f,
+		1.f, 1.f, 0.f,
+		1.f, 1.f, 0.f,
+		1.f, 1.f, 0.f,
+		1.f, 1.f, 0.f,
 	};
 	//VBO -- VERTEX
 	static const GLfloat g_vertex_buffer_data[] = {
@@ -744,42 +745,78 @@ void SetupGL() //
 		1.0f,-1.0f, 1.0f
 	};
 	static const GLfloat g_color_buffer_data[] = {
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-		0.597f,  0.770f,  0.761f,
-		0.559f,  0.436f,  0.730f,
-		0.359f,  0.583f,  0.152f,
-		0.483f,  0.596f,  0.789f,
-		0.559f,  0.861f,  0.639f,
-		0.195f,  0.548f,  0.859f,
-		0.014f,  0.184f,  0.576f,
-		0.771f,  0.328f,  0.970f,
-		0.406f,  0.615f,  0.116f,
-		0.676f,  0.977f,  0.133f,
-		0.971f,  0.572f,  0.833f,
-		0.140f,  0.616f,  0.489f,
-		0.997f,  0.513f,  0.064f,
-		0.945f,  0.719f,  0.592f,
-		0.543f,  0.021f,  0.978f,
-		0.279f,  0.317f,  0.505f,
-		0.167f,  0.620f,  0.077f,
-		0.347f,  0.857f,  0.137f,
-		0.055f,  0.953f,  0.042f,
-		0.714f,  0.505f,  0.345f,
-		0.783f,  0.290f,  0.734f,
-		0.722f,  0.645f,  0.174f,
-		0.302f,  0.455f,  0.848f,
-		0.225f,  0.587f,  0.040f,
-		0.517f,  0.713f,  0.338f,
-		0.053f,  0.959f,  0.120f,
-		0.393f,  0.621f,  0.362f,
-		0.673f,  0.211f,  0.457f,
-		0.820f,  0.883f,  0.371f,
-		0.982f,  0.099f,  0.879f
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+		0.f, 0.f, 1.f,
+	};
+	static const GLfloat g_color_buffer_data_hip[] = {
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 1.f, 0.f,
 	};
 
 	// Generate 1 buffer, put the resulting identifier in vertexbuffer
@@ -795,6 +832,10 @@ void SetupGL() //
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 	// Give our vertices to OpenGL.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &hipcolorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, hipcolorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data_hip), g_color_buffer_data_hip, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &legbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, legbuffer);
